e8aea34413105dea098c7a9ed6b25c38ac9d0136
Taskkill <johny95wil@gmail.com>
2020-02-16 00:40:34 +0100

    minor import refactor

--- src/SystemF/TypeChecker.hs
+++ src/SystemF/TypeChecker.hs
@@ -1,61 +1,63 @@
 module SystemF.TypeChecker where
 
-import qualified SystemF.AST as AST
-import qualified SystemF.Types as Type
+import SystemF.AST (Expression(..))
+import qualified SystemF.Types as T
 
-type Context = [(String, Type.Type)]
 
-typeOfTerm :: String -> Either Type.Type String
+type Context = [(String, T.Type)]
+
+
+typeOfTerm :: String -> Either T.Type String
 -- TODO: more of them later?
-typeOfTerm "+" = Left $ Type.Arr Type.Nat (Type.Arr Type.Nat Type.Nat)
-typeOfTerm "T" = Left Type.Boolean
-typeOfTerm "F" = Left Type.Boolean
+typeOfTerm "+" = Left $ T.Arr T.Nat (T.Arr T.Nat T.Nat)
+typeOfTerm "T" = Left T.Boolean
+typeOfTerm "F" = Left T.Boolean
 typeOfTerm _ = Right "Term has no type."
 
-addType :: String -> Type.Type -> Context -> Context
+addType :: String -> T.Type -> Context -> Context
 addType var type' context = (var, type') : context
 
-getType :: String -> Context -> Either Type.Type String
+getType :: String -> Context -> Either T.Type String
 getType _ [] = Right "Free Variable has unknown type."
 getType name ((var, type') : ctx)
   | name == var = Left type'
   | otherwise = getType name ctx
 
-typeOf :: AST.Expression -> Either Type.Type String
+typeOf :: Expression -> Either T.Type String
 typeOf exp = typeOf' exp []
 
-typeOf' :: AST.Expression -> Context -> Either Type.Type String
-typeOf' (AST.Natural n) _ = Left Type.Nat
-typeOf' (AST.Boolean b) _ = Left Type.Boolean
-typeOf' (AST.Operator op) _ = typeOfTerm op
-typeOf' (AST.Macro t) _ = typeOfTerm t
-typeOf' (AST.Variable v) ctx = getType v ctx
-typeOf' (AST.Abstraction arg t body) ctx =
+typeOf' :: Expression -> Context -> Either T.Type String
+typeOf' (Natural n) _ = Left T.Nat
+typeOf' (Boolean b) _ = Left T.Boolean
+typeOf' (Operator op) _ = typeOfTerm op
+typeOf' (Macro t) _ = typeOfTerm t
+typeOf' (Variable v) ctx = getType v ctx
+typeOf' (Abstraction arg t body) ctx =
   let t' = lookUp t ctx
       newEnv = addType arg t' ctx
       bodyt  = typeOf' body newEnv in
       case bodyt of
-        Left t'' -> Left $ Type.Arr t' t''
+        Left t'' -> Left $ T.Arr t' t''
         Right e -> Right e
-typeOf' (AST.Application left right) ctx =
+typeOf' (Application left right) ctx =
   let lt = typeOf' left ctx
       rt = typeOf' right ctx in
       case lt of
-        Left (Type.Arr a b) | Left a == rt -> Left b
-        Left (Type.Arr a b) -> Right $ "Type mismatch in: " ++ show a ++ " applied to" ++ show b ++ "." -- type mismatch
+        Left (T.Arr a b) | Left a == rt -> Left b
+        Left (T.Arr a b) -> Right $ "Type mismatch in: " ++ show a ++ " applied to" ++ show b ++ "." -- type mismatch
         Right e -> Right e
         -- _ -> Right
-typeOf' (AST.TypeApplication (AST.TypeAbstraction par exp) type') ctx =
+typeOf' (TypeApplication (TypeAbstraction par exp) type') ctx =
   typeOf' exp $ addType par type' ctx
-typeOf' (AST.TypeAbstraction par exp) ctx =
+typeOf' (TypeAbstraction par exp) ctx =
   case typeOf' exp ctx of
-    Left t -> Left (Type.ForAll par t)
+    Left t -> Left (T.ForAll par t)
     Right e -> Right e
 
-lookUp :: Type.Type -> Context -> Type.Type
-lookUp (Type.Parameter name) ctx =
+lookUp :: T.Type -> Context -> T.Type
+lookUp (T.Parameter name) ctx =
   case getType name ctx of
     Left t -> t
-    Right e -> Type.Parameter name
-lookUp (Type.Arr t1 t2) ctx = Type.Arr (lookUp t1 ctx) (lookUp t2 ctx)
+    Right e -> T.Parameter name
+lookUp (T.Arr t1 t2) ctx = T.Arr (lookUp t1 ctx) (lookUp t2 ctx)
 lookUp type' _ = type'
