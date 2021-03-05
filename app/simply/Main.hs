8dc4a395b193a64faf698bb61dc15a125d8156bb
Taskkill <johny95wil@gmail.com>
2021-03-05 15:59:56 +0100

    refactor the repl, it should be more usable now

--- app/simply/Main.hs
+++ app/simply/Main.hs
@@ -11,51 +11,71 @@ import Simply.TypeChecker (typeOf)
 
 main :: IO ()
 main = do
-  putStrLn "[enter λ-> expression]"
-  line <- prompt ":$ "
-  let ast = fst $ last $ readP_to_S expression line
-  execCommand ast
+  putStrLn "Welcome to the λ-> REPL"
+  putStrLn "type:"
+  putStrLn "  :step       for the next step of the evaluation"
+  putStrLn "  :type       for type annotation of the current expression"
+  putStrLn "  :isnormal   for checking if the expression is in the normal form"
+  putStrLn "  :applyto    for adding an argument to the current expression"
+  putStrLn "  :normalize  for the evaluation until the normal form is reached"
+  putStrLn "  :reset      for restarting the REPL state"
+  putStrLn "  :bye        for exiting the program"
+
+  putStrLn "\n[enter λ-> expression]"
+  line <- prompt "λ-> >> "
+  case line of
+    ":bye" -> return ()
+    _ -> do
+      let ast = fst $ last $ readP_to_S expression line
+      execCommand ast
+
 
 execCommand :: Expression -> IO ()
 execCommand exp = do
-  cmnd <- prompt "[command or expression]:$ "
+  present'expr exp
+  putStrLn "[enter command or λ-> expression]"
+  cmnd <- prompt "λ-> >> "
   case cmnd of
     ":step" -> do
       let next = normalStep exp
-      putStr ":$ "
       print next
       execCommand next
     ":normalize" -> do
       let normal = normalize exp
-      putStr ":$ "
-      print normal
       execCommand normal
-    ":new" -> main
-    ":print" -> do
-      print exp
-      execCommand exp
+    ":reset" -> main
     ":type" -> do
       putStr ":: "
       putStrLn $ unwrapType (typeOf exp)
       execCommand exp
     ":isnormal" -> do
-      print $ normalForm exp
+      putStrLn $ is'normal $ normalForm exp
       execCommand exp
     ":applyto" -> do
-      putStrLn "[enter λ-> expression]"
-      line <- prompt ":$ "
+      line <- prompt "λ-> >> "
       let ast = fst $ last $ readP_to_S expression line
       execCommand $ Application exp ast
     ":bye" -> return ()
     _ -> execCommand $ fst $ last $ readP_to_S expression cmnd
 
+
+present'expr :: Expression -> IO ()
+present'expr expr = do
+  putStrLn $ "       " ++ show expr ++ " :: " ++ unwrapType (typeOf expr)
+
+
+is'normal :: Bool -> String
+is'normal False = "       Not in the Normal Form yet."
+is'normal True = "       Reached the Normal Form."
+
+
 unwrapType :: Either Type String -> String
 unwrapType (Left t) = show t
 unwrapType (Right e) = "~" ++ e ++ "~"
 
-prompt :: String -> IO (String)
+
+prompt :: String -> IO String
 prompt msg = do
   putStr msg
   hFlush stdout
-  line <- getLine
-  return line
+  getLine
